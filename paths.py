2555e2976d0f19041c628ef14c4830f0b5e7f288
dvornikita <dvornik.nikita@gmail.com>
2019-09-18 14:07:51 +0200

    my paths removed

--- paths.py
+++ paths.py
@@ -2,8 +2,11 @@ import os
 import sys
 
 PROJECT_ROOT = '/'.join(os.path.realpath(__file__).split('/')[:-1])
-OMNIGLOT_ROOT = '/home/mdvornik/scratch/Git/prototypical-networks/data/omniglot/'
-MINI_IMAGENET_ROOT = '/home/mdvornik/gpuhost9_scratch/Datasets/mini_imagenet_fullres'
-CUB_ROOT = '/home/mdvornik/gpuhost9_scratch/Datasets/CUB_cropped'
-TIERED_IMAGENET_ROOT = '/home/mdvornik/gpuhost9_scratch/Datasets/tiered_imagenet_fullres'
-COCO_ROOT = '/home/mdvornik/gpuhost9_scratch/Datasets/COCO'
+IMAGENET_ROOT = None    # give a path to the original imagenet
+MINI_IMAGENET_ROOT = IMAGENET_ROOT
+
+# In case you want to work with these datasets too...
+OMNIGLOT_ROOT = None    # put your path here if you want to use this dataset
+CUB_ROOT = None    # put your path here if you want to use this dataset
+TIERED_IMAGENET_ROOT = None    # put your path here if you want to use this dataset
+COCO_ROOT = None    # put your path here if you want to use this dataset
